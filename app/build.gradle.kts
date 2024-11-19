6518cc3da9f775785907fc313cc231ac1f4fc52c
myduyen04 <myduyen041104@gmail.com>
2024-11-19 16:32:48 +0700

    Giỏ hàng

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,10 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.inappmessaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
 }
