529d50f6de0af7a8de230dab19843e5b57a75a7f
Thùy Trang <phamthuytrang31204@gmail.com>
2024-12-03 23:20:16 +0700

    Home

---
+++ app/src/main/java/com/nmq/foodninjaver2/features/Home/MenuDetail/MenuDetailActivity.java
@@ -0,0 +1,26 @@
+package com.nmq.foodninjaver2.features.Home.MenuDetail;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.nmq.foodninjaver2.R;
+
+public class MenuDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu_detail);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
